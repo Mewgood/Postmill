bfec4897de251eaf43404284bcb10abaa94675bc
Emma <emma1312@protonmail.ch>
2019-04-18 13:20:32 +0000

    update vulnerable dependencies/fix XSS in search

--- src/Twig/FormattingExtension.php
+++ src/Twig/FormattingExtension.php
@@ -19,6 +19,9 @@ final class FormattingExtension extends AbstractExtension {
 
     public function getFilters(): array {
         return [
+            new TwigFilter('search_highlight', static function (string $html) {
+                return preg_replace('!&lt;b&gt;(.*?)&lt;/b&gt;!', '<b>\1</b>', $html);
+            }, ['is_safe' => ['html'], 'pre_escape' => 'html']),
             new TwigFilter('markdown', [$this->markdownConverter, 'convertToHtml']),
             new TwigFilter('cached_markdown', [$this->markdownConverter, 'convertToHtmlCached']),
             new TwigFilter('slugify', Slugger::class.'::slugify'),
