8779e1c0e6f6916a8cbc36ffad496ecc1ac70e1f
Emma <emma1312@protonmail.ch>
2019-10-04 01:03:22 +0200

    refactor alerts

--- assets/js/alerts.js
+++ assets/js/alerts.js
@@ -1,14 +1,12 @@
 import $ from 'jquery';
 
-$(document).on('click', '.site-alerts__alert', function () {
-    const $alert = $(this);
+$(document).on('click', '.site-alerts__dismiss', function () {
+    const $alert = $(this).parents('.site-alerts__alert');
 
     $alert
         .removeClass('site-alerts__alert')
         .each((i, el) => el.offsetWidth)
         .addClass('site-alerts__alert')
         .css('animation-direction', 'reverse')
-        .one('animationend', function () {
-            $alert.remove();
-        });
+        .one('animationend', () => $alert.remove());
 });
