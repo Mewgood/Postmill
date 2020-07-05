adb71444b6743ac282b093bd35f1a973668fb020
Emma <emma1312@protonmail.ch>
2020-07-05 12:43:29 +0200

    log to /dev/null in console when log file = stderr

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,3 +5,7 @@ use Symfony\Component\Dotenv\Dotenv;
 require dirname(__DIR__).'/vendor/autoload.php';
 
 (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+
+if ($_SERVER['LOG_FILE'] === 'php://stderr') {
+    putenv('LOG_FILE='.$_SERVER['LOG_FILE'] = $_ENV['LOG_FILE'] = PATH_SEPARATOR === '\\' ? 'NUL' : '/dev/null');
+}
