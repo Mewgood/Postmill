7d09ad5f6d8298bc453256547a29e7474a0c5959
Emma <emma1312@protonmail.ch>
2020-07-22 00:01:31 +0200

    prettify forum select box

--- assets/js/select2.js
+++ assets/js/select2.js
@@ -1,3 +1,28 @@
+import icon from './lib/icon';
+
+const forumSelectorTemplate = ({ loading, element, text }, $) => {
+    if (loading) {
+        return '';
+    }
+
+    const $nameEl = $('<span class="flex__grow" />')
+        .text(element.getAttribute('data-name') || text);
+
+    const $iconEl = $('<span />');
+
+    if (element.getAttribute('data-subscribed')) {
+        $iconEl.append(icon('heart', { extraClasses: 'fg-red' }));
+    }
+
+    if (element.getAttribute('data-featured')) {
+        $iconEl.append(icon('star', { extraClasses: 'fg-orange' }));
+    }
+
+    return $('<span class="flex" />')
+        .append($nameEl)
+        .append($iconEl);
+};
+
 document.querySelectorAll('.select2').forEach(async el => {
     const { default: $ } = await import('jquery');
 
@@ -10,5 +35,12 @@ document.querySelectorAll('.select2').forEach(async el => {
         import('select2/dist/css/select2.css'),
     ]);
 
-    $(el).select2();
+    const options = {};
+
+    if (el.getAttribute('data-forum-selector')) {
+        options.templateResult = state => forumSelectorTemplate(state, $);
+        options.templateSelection = state => forumSelectorTemplate(state, $);
+    }
+
+    $(el).select2(options);
 });
